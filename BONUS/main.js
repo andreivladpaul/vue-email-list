344c0d10473e76a614ae7c288e8ba5c467a00769
Vlad Andrei <andreivladpaul@gmail.com>
2021-09-16 15:37:04 +0200

    BONUS

---
+++ BONUS/main.js
@@ -0,0 +1,35 @@
+Vue.config.devtools = true;
+
+const app = new Vue({
+    el : '#app',
+    
+    data : {
+        emailList: [],
+        email: '',
+        counterMail: 10,
+        show: false
+
+        
+        
+    },
+    methods: {
+        createEmails() {
+            for(let i = 0; i < this.counterMail; i++ )
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((result) => {
+                this.email = result.data.response
+                this.emailList.push(this.email)
+            })
+        }
+
+    
+    }, 
+    mounted() {
+        this.createEmails()
+    },
+
+    
+    
+   
+}) 
